5d432125567a786824bf45b33aaab759d181b4f5
Leonardo Ferreira Nascimento <38327786+NascLeonardo@users.noreply.github.com>
2025-02-24 19:43:17 -0300

    Delete PythonProject2/my_tennis_club directory

--- PythonProject2/my_tennis_club/equipments/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Equipment(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.CharField(max_length=255)
-    quantity = models.IntegerField()
--- PythonProject2/my_tennis_club/equipments/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns =[
-    path('equipments/', views.equipments, name='equipments'),
-    path('equipments/details/<int:id>', views.details  , name='equipments'),
-]
